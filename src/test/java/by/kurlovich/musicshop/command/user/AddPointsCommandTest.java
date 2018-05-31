e74814f11e80bcdb7c084e0cfa5c3eb4635196fd
Nik <qbk@tut.by>
2018-05-31 06:11:35 +0300

    test updated

--- src/test/java/by/kurlovich/musicshop/command/user/AddPointsCommandTest.java
+++ src/test/java/by/kurlovich/musicshop/command/user/AddPointsCommandTest.java
@@ -25,20 +25,21 @@ public class AddPointsCommandTest {
     @Test
     public void testUnAuthorisedUser() throws CommandException {
         AddPointsCommand command = new AddPointsCommand(userReceiver);
-        //setting expectations
+
         new Expectations(command) {{
             command.getUserRole(withNotNull());
             result = "not_valid_role";
         }};
-        //execute
+
         CommandResult result = command.execute(request);
-        //assert/verify
+
         new Verifications() {{
             command.isAuthorised(withNotNull());
             times = 1;
             request.setAttribute("message", "denied");
             times = 1;
         }};
+
         Assert.assertEquals(result.getPage(), PageStore.ERROR_PAGE.getPageName());
         Assert.assertEquals(result.getResponseType(), CommandResult.ResponseType.FORWARD);
     }
@@ -48,7 +49,7 @@ public class AddPointsCommandTest {
         AddPointsCommand command = new AddPointsCommand(userReceiver);
         final int startPoints = 100;
         User user = createUser(startPoints);
-        //setting expectations
+
         new Expectations(command) {{
             command.isAuthorised(withNotNull());
             result = true;
@@ -59,18 +60,81 @@ public class AddPointsCommandTest {
             userReceiver.updateUser(withNotNull());
             result = true;
         }};
-        //execute
+
         CommandResult result = command.execute(request);
-        //assert/verify
+
         new Verifications() {{
             request.setAttribute("message", withNotNull());
             times = 0;
             userReceiver.updateUser(withNotNull());
             times = 1;
         }};
+
         Assert.assertEquals(result.getPage(), PageStore.POINTS_PAGE.getPageName());
         Assert.assertEquals(result.getResponseType(), CommandResult.ResponseType.REDIRECT);
         Assert.assertEquals(user.getPoints(), startPoints + Tariff.MIN.getPoints());
+    }
+
+    @Test
+    public void testOptTariffAdd() throws CommandException, ReceiverException {
+        AddPointsCommand command = new AddPointsCommand(userReceiver);
+        final int startPoints = 100;
+        User user = createUser(startPoints);
+
+        new Expectations(command) {{
+            command.isAuthorised(withNotNull());
+            result = true;
+            command.getTariffSubmitted(withNotNull());
+            result = Tariff.OPT;
+            command.getCurrentUser(withNotNull());
+            result = user;
+            userReceiver.updateUser(withNotNull());
+            result = true;
+        }};
+
+        CommandResult result = command.execute(request);
+
+        new Verifications() {{
+            request.setAttribute("message", withNotNull());
+            times = 0;
+            userReceiver.updateUser(withNotNull());
+            times = 1;
+        }};
+
+        Assert.assertEquals(result.getPage(), PageStore.POINTS_PAGE.getPageName());
+        Assert.assertEquals(result.getResponseType(), CommandResult.ResponseType.REDIRECT);
+        Assert.assertEquals(user.getPoints(), startPoints + Tariff.OPT.getPoints());
+    }
+
+    @Test
+    public void testMaxTariffAdd() throws CommandException, ReceiverException {
+        AddPointsCommand command = new AddPointsCommand(userReceiver);
+        final int startPoints = 100;
+        User user = createUser(startPoints);
+
+        new Expectations(command) {{
+            command.isAuthorised(withNotNull());
+            result = true;
+            command.getTariffSubmitted(withNotNull());
+            result = Tariff.MAX;
+            command.getCurrentUser(withNotNull());
+            result = user;
+            userReceiver.updateUser(withNotNull());
+            result = true;
+        }};
+
+        CommandResult result = command.execute(request);
+
+        new Verifications() {{
+            request.setAttribute("message", withNotNull());
+            times = 0;
+            userReceiver.updateUser(withNotNull());
+            times = 1;
+        }};
+
+        Assert.assertEquals(result.getPage(), PageStore.POINTS_PAGE.getPageName());
+        Assert.assertEquals(result.getResponseType(), CommandResult.ResponseType.REDIRECT);
+        Assert.assertEquals(user.getPoints(), startPoints + Tariff.MAX.getPoints());
     }
 
     private User createUser(int points) {
