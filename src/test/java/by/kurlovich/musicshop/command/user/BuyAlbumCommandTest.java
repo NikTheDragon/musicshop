153629c109c27ff3d263da3ce0939f7a4348a0fd
Nik <qbk@tut.by>
2018-06-04 20:37:02 +0300

    test added

---
+++ src/test/java/by/kurlovich/musicshop/command/user/BuyAlbumCommandTest.java
@@ -0,0 +1,80 @@
+package by.kurlovich.musicshop.command.user;
+
+import by.kurlovich.musicshop.command.CommandException;
+import by.kurlovich.musicshop.entity.User;
+import by.kurlovich.musicshop.receiver.ReceiverException;
+import by.kurlovich.musicshop.receiver.UserReceiver;
+import by.kurlovich.musicshop.web.CommandResult;
+import by.kurlovich.musicshop.web.pages.PageStore;
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.Verifications;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class BuyAlbumCommandTest {
+
+    @Mocked
+    UserReceiver userReceiver;
+
+    @Mocked
+    HttpServletRequest request;
+
+    @Test
+    public void testUnAuthorisedUser() throws CommandException {
+        BuyAlbumCommand command = new BuyAlbumCommand(userReceiver);
+
+        new Expectations(command) {{
+            command.getUserRole(withNotNull());
+            result = "not_valid_role";
+        }};
+
+        CommandResult result = command.execute(request);
+
+        new Verifications() {{
+            command.isAuthorised(withNotNull());
+            times = 1;
+            request.setAttribute("message", "denied");
+            times = 1;
+        }};
+
+        Assert.assertEquals(result.getPage(), PageStore.ERROR_PAGE.getPageName());
+        Assert.assertEquals(result.getResponseType(), CommandResult.ResponseType.FORWARD);
+    }
+
+    @Test
+    public void testInvalidBuyAlbum() throws CommandException, ReceiverException {
+        BuyAlbumCommand command = new BuyAlbumCommand(userReceiver);
+        User user = createUser();
+
+        new Expectations(command) {{
+            command.isAuthorised(withNotNull());
+            result = true;
+            command.getCurrentUser(withNotNull());
+            result = user;
+            request.getParameter("album_id");
+            result = "1";
+            request.getParameter("album_price");
+            result = "1";
+            userReceiver.buyAlbum(withNotNull(), "1", 1);
+            result = "false";
+        }};
+
+        CommandResult result = command.execute(request);
+
+        new Verifications() {{
+            request.setAttribute("message", "false");
+            times = 1;
+        }};
+
+        Assert.assertEquals(result.getPage(), PageStore.ERROR_PAGE.getPageName());
+        Assert.assertEquals(result.getResponseType(), CommandResult.ResponseType.FORWARD);
+    }
+
+    private User createUser() {
+        User user = new User();
+        return user;
+    }
+}
